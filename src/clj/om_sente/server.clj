51f5ecb2d839e950fa0422858804b87ce767da9a
Sean Corfield <sean@corfield.org>
2014-04-10 15:32:39 -0700

    Get secured application example working; clean up code; add comments / docstrings.

--- src/clj/om_sente/server.clj
+++ src/clj/om_sente/server.clj
@@ -1,3 +1,9 @@
+;; copyright (c) 2014 Sean Corfield
+;;
+;; small demo to show Om / Sente playing together
+;;
+;; no claim is made of best practices - feedback welcome
+
 (ns om-sente.server
   (:require [clojure.core.async :as async
              :refer [<! <!! chan go]]
@@ -7,34 +13,46 @@
             [org.httpkit.server :as kit]
             [taoensso.sente :as s]))
 
+;; create the Sente web socket connection stuff when we are loaded:
+
 (let [{:keys [ch-recv send-fn ajax-post-fn
               ajax-get-or-ws-handshake-fn] :as sente-info}
       (s/make-channel-socket! {})]
-  (println "SENTE:" sente-info)
   (def ring-ajax-post   ajax-post-fn)
   (def ring-ajax-get-ws ajax-get-or-ws-handshake-fn)
   (def ch-chsk          ch-recv)
-  (def chsk-send!       send-fn)
-  (println "defs in place"))
+  (def chsk-send!       send-fn))
 
 (defn root
+  "Return the absolute (root-relative) version of the given path."
   [path]
-  (println "user.dir" (System/getProperty "user.dir"))
   (str (System/getProperty "user.dir") path))
 
 (defn unique-id
-  "Return a really unique ID (for an unsecured session ID)."
+  "Return a really unique ID (for an unsecured session ID).
+  No, a random number is not unique enough. Use a UUID for real!"
   []
   (rand-int 10000))
+
+(defn session-uid
+  "Convenient to extract the UID that Sente needs from the request."
+  [req]
+  (get-in req [:session :uid]))
 
 (defn index
   "Handle index page request. Injects session uid if needed."
   [req]
   {:status 200
-   :session (if (get-in req [:session :uid])
+   :session (if (session-uid req)
               (:session req)
               (assoc (:session req) :uid (unique-id)))
    :body (slurp "index.html")})
+
+;; minimal set of routes to handle:
+;; - home page request
+;; - web socket GET/POST
+;; - general files (mainly JS)
+;; - 404
 
 (defroutes server
   (-> (routes
@@ -45,50 +63,58 @@
        (r/not-found "<p>Page not found. I has a sad!</p>"))
       h/site))
 
-(defmulti handle-event (fn [event ring-req] (first event)))
+(defmulti handle-event
+  "Handle events based on the event ID."
+  (fn [[ev-id ev-arg] ring-req] ev-id))
 
-(defmethod handle-event :test/echo
-  [[_ msg] req]
-  (chsk-send! (get-in req [:session :uid]) [:test/reply (str "<" msg ">")]))
-
-(defmethod handle-event :default
-  [_ req]
-  nil)
-
-;; :session/status -> :session/state :open / :secure
-;; :session/auth username password -> :auth/fail or :auth/success
-;; (updates session uid etc)
+;; Reply with the session state - either open or secure.
+;; Note: this doesn't work yet since :token is never added to the session!
 
 (defmethod handle-event :session/status
   [_ req]
-  (chsk-send! (get-in req [:session :uid])
+  (chsk-send! (session-uid req)
               [:session/state (if (get-in req [:session :token])
                                 :secure
                                 :open)]))
 
+;; Reply with authentication failure or success.
+;; This should update the session with :token but Sente doesn't allow session updates.
+
 (defmethod handle-event :session/auth
-  [[_ username password] req]
-  (chsk-send! (get-in req [:session :uid])
-              [(if (and (= "admin" username)
-                        (= "secret" password))
-                 :auth/succcess
-                 :auth/fail)]))
+  [[_ [username password]] req]
+  (let [valid (and (= "admin" username)
+                   (= "secret" password))]
+    (chsk-send! (session-uid req)
+                [(if valid :auth/success :auth/fail)])))
 
-(defn handle-data
-  "Main event processor."
-  [{:keys [client-uuid ring-req event] :as data}]
-  (println data)
-  (handle-event event ring-req))
+;; Reply with the message in angle brackets.
 
-(defn -main [& args]
-  (println "starting -main")
-  (go (loop []
-        (println "waiting for data...")
-        (handle-data (<! ch-chsk))
-        (recur)))
-  (println "about to start server")
+(defmethod handle-event :test/echo
+  [[_ msg] req]
+  (chsk-send! (session-uid req) [:test/reply (str "<" msg ">")]))
+
+;; Handle unknown events.
+;; Note: this includes the Sente implementation events like:
+;; - :chsk/ping
+;; - :chsk/uidport-open
+;; - :chsk/uidport-close
+
+(defmethod handle-event :default
+  [event req]
+  nil)
+
+(defn event-loop
+  "Handle inbound events."
+  []
+  (go (loop [{:keys [client-uuid ring-req event] :as data} (<! ch-chsk)]
+        (handle-event event ring-req)
+        (recur (<! ch-chsk)))))
+
+(defn -main
+  "Start the http-kit server. Takes no arguments.
+  Environment variable PORT can override default port of 8444."
+  [& args]
+  (event-loop)
   (let [port (or (System/getenv "PORT") 8444)]
+    (println "Starting Sente server on port" port "...")
     (kit/run-server #'server {:port port})))
-
-(println "server loaded")
-;; (-main)
--- src/cljs/om_sente/core.cljs
+++ src/cljs/om_sente/core.cljs
@@ -1,3 +1,9 @@
+;; copyright (c) 2014 Sean Corfield
+;;
+;; small demo to show Om / Sente playing together
+;;
+;; no claim is made of best practices - feedback welcome
+
 (ns om-sente.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
@@ -7,130 +13,174 @@
 
 (enable-console-print!)
 
+;; create the Sente web socket connection stuff when we are loaded:
+
 (let [{:keys [chsk ch-recv send-fn]}
       (s/make-channel-socket! "/qw" {} {:type :auto})]
   (def chsk       chsk)
   (def ch-chsk    ch-recv)
   (def chsk-send! send-fn))
 
-(defn send-field [e owner state]
+(defn field-change
+  "Generic input field updater. Keeps state in sync with input."
+  [e owner field]
+  (let [value (.. e -target -value)]
+    (om/set-state! owner field value)))
+
+(defn send-text-on-enter
+  "When user presses ENTER, send the reversed value of the field to the server
+  and clear the field's input state."
+  [e owner state]
   (when (== (.-keyCode e) 13)
     (chsk-send! [:test/echo (clojure.string/reverse (:text state))])
     (om/set-state! owner :text "")))
 
-(defn field-change [e owner state]
-  (let [value (.. e -target -value)]
-    (om/set-state! owner :text value)))
-
-(defn field-view [app owner opts]
+(defn text-sender
+  "Component that displays a text field and sends it to the server when ENTER is pressed."
+  [app owner]
   (reify
     om/IInitState
     (init-state [this]
-      {:text ""})
+                {:text ""})
     om/IRenderState
     (render-state [this state]
-      (dom/input #js {:ref (:name opts) :type (or (:type opts) "text") :value (:text state)
-                      :onChange #(field-change % owner state)
-                      :onKeyPress #(send-field % owner state)}))))
+                  (dom/input #js {:type "text" :value (:text state)
+                                  :onChange #(field-change % owner :text)
+                                  :onKeyPress #(send-text-on-enter % owner state)}))))
 
-(defmulti handle-event (fn [event app owner] (first event)))
+(defn data-view
+  "Component that displays the data (text) returned by the server after processing."
+  [app owner]
+  (reify
+    om/IRender
+    (render [this]
+            (dom/div nil (:data/text app)))))
+
+(defmulti handle-event
+  "Handle events based on the event ID."
+  (fn [[ev-id ev-arg] app owner] ev-id))
+
+;; Process the server's reply by updating the application state:
 
 (defmethod handle-event :test/reply
   [[_ msg] app owner]
-  (om/set-state! owner :text msg))
+  (om/update! app :data/text msg))
+
+;; Ignore unknown events (we just print to the console):
 
 (defmethod handle-event :default
-  [_ app owner]
-  nil)
+  [event app owner]
+  (println "UNKNOWN EVENT" event))
+
+;; Remember the session state in the application component's local state:
 
 (defmethod handle-event :session/state
   [[_ state] app owner]
-  (js/alert (str "session/state: " (name state)))
   (om/set-state! owner :session/state state))
+
+;; Handle authentication failure (we just set an error message for display):
 
 (defmethod handle-event :auth/fail
   [_ app owner]
   (om/update! app [:notify/error] "Invalid credentials"))
+
+;; Handle authentication success (clear the error message; update application session state):
 
 (defmethod handle-event :auth/success
   [_ app owner]
   (om/update! app [:notify/error] nil)
   (om/set-state! owner :session/state :secure))
 
-(defn event-loop [app owner]
-  (go (loop []
-        (let [[op arg] (<! ch-chsk)]
-          (case op
-            :chsk/recv (handle-event arg app owner)
-            nil))
-        (recur))))
+(defn test-session
+  "If we don't know the session state, ask the server for it."
+  [owner]
+  (when (= :unknown (om/get-state owner :session/state))
+    (chsk-send! [:session/status])))
 
-(defn data-view [app owner]
+(defn event-loop
+  "Handle inbound events."
+  [app owner]
+  (go (loop [[op arg] (<! ch-chsk)]
+        (case op
+          :chsk/recv (handle-event arg app owner)
+          ;; we ignore other Sente events
+          nil)
+        (test-session owner)
+        (recur (<! ch-chsk)))))
+
+(defn attempt-login
+  "Handle the login event - send credentials to the server."
+  [e app owner]
+  (let [username (-> (om/get-node owner "username") .-value)
+        password (-> (om/get-node owner "password") .-value)]
+    (chsk-send! [:session/auth [username password]]))
+  ;; suppress the form submit:
+  false)
+
+(defn login-form
+  "Component that provides a login form and submits credentials to the server."
+  [app owner]
   (reify
     om/IInitState
     (init-state [this]
-      {:text "none"})
+                {:username "" :password ""})
     om/IRenderState
-    (render-state [this {:keys [text]}]
-      (dom/div nil text))))
+    (render-state [this state]
+                  (dom/div #js {:style {:align "center" :width "25%"}}
+                           (if-let [error (:notify/error app)]
+                             (dom/div #js {:style {:color "red"}}
+                                      error))
+                           (dom/h1 nil "Login")
+                           (dom/form #js {:onSubmit #(attempt-login % app owner)}
+                                     (dom/div #js {:style {:width "100%"}}
+                                              (dom/p nil "Username")
+                                              (dom/input #js {:ref "username" :type "text" :value (:username state)
+                                                              :onChange #(field-change % owner :username)}))
+                                     (dom/div #js {:style {:width "100%"}}
+                                              (dom/p nil "Password")
+                                              (dom/input #js {:ref "password" :type "password" :value (:password state)
+                                                              :onChange #(field-change % owner :password)}))
+                                     (dom/div #js {:style {:width "50%" :align "center"}}
+                                              (dom/input #js {:type "submit" :value "Login"})))))))
 
-(defn login [e app owner]
-  nil)
-
-(defn login-view [app owner]
+(defn secured-application
+  "Component that represents the secured portion of our application."
+  [app owner]
   (reify
     om/IRender
     (render [this]
-            (dom/div #js {:style {:align "center" :width "25%"}}
-                     (if-let [error (:notify/error app)]
-                       (dom/div #js {:style "color: red;"}
-                                error))
-                     (dom/h1 nil "Login")
-                     (dom/div #js {:style {:width "100%"}}
-                             (dom/p nil "Username")
-                             (om/build field-view app {:opts {:name "username"}}))
-                     (dom/div #js {:style {:width "100%"}}
-                             (dom/p nil "Password")
-                             (om/build field-view app {:opts {:name "password"
-                                                              :type "password"}}))
-                     (dom/div #js {:style {:width "50%" :align "center"}}
-                             (dom/button #js {:onClick #(login % app owner)}
-                                        "Login"))))))
+            (dom/div nil
+                     (dom/h1 nil "Test Sente")
+                     (om/build text-sender app {})
+                     (om/build data-view app {})))))
 
-(defn test-session [owner]
-  (when (= :unknown (om/get-state owner :session/state))
-    (js/alert "will-mount send session/status query")
-    (chsk-send! [:session/status])))
-
-(defn app-view [app owner]
+(defn application
+  "Component that represents our application. Maintains session state.
+  Selects views based on session state."
+  [app owner]
   (reify
     om/IInitState
     (init-state [this]
-      {:session/state :unknown})
+                {:session/state :unknown})
     om/IWillMount
     (will-mount [this]
-      (event-loop app owner))
-    om/IDidMount
-    (did-mount [this]
-      (test-session owner))
+                (event-loop app owner))
     om/IRenderState
     (render-state [this state]
-      (case (:session/state state)
-        :secure
-        (dom/div nil
-                 (dom/h1 nil "Test Sente")
-                 (om/build field-view app {:opts {:name "data"}})
-                 (om/build data-view app {}))
-        :open
-        (om/build login-view app {})
-        :unknown
-        (dom/div nil "Loading...")
-        ;;(om/build login-view app {})
-        ))))
+                  (dom/div #js {:style {:width "100%"}}
+                           (case (:session/state state)
+                             :secure
+                             (om/build secured-application app {})
+                             :open
+                             (om/build login-form app {})
+                             :unknown
+                             (dom/div nil "Loading..."))))))
 
-(def app-state (atom {}))
+(def app-state
+  "Our very minimal application state - a piece of text that we display."
+  (atom {:data/text "none"}))
 
-(om/root app-view
+(om/root application
          app-state
          {:target (. js/document (getElementById "app"))})
 
